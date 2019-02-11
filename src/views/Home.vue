df667ee932d6d3da083ae934357f8cbb73e5dbf6
Ryan Hightower <ryan@ryanhightower.com>
2019-02-11 04:27:24 -0700

    Begin "Share Feedback" start page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,47 @@
 <template>
-  <div class="home">
-    <h1 class="title is-1">Question Types</h1>
-    <h3 class="title is-5">Free Form</h3>
-    <QuestionOpen />
-    <br>
-    <h3 class="title is-5">Multiple Choice</h3>
-    <QuestionMultipleChoice />
-    <br>
-    <h3 class="title is-5">Scale</h3>
-    <QuestionScale />
+  <div class="home container">
+
+    <h1 class="title is-3">Share Feedback</h1>
+    <div class="container" style="max-width: 600px; border: 1px solid #eee; border-radius: 3px;">
+    <div
+      v-for="user in users"
+      :key="user.id"
+      class="level"
+      style="margin: 0; padding: 20px; border-bottom: 1px solid #eee;">
+      <div class="level-left">
+        <!-- <figure class="media-left"> -->
+          <img :src="user.avatar" alt="" class="level-item">
+        <!-- </figure> -->
+        <h3 class="level-item title is-4">{{ user.name }}</h3>
+      </div>
+      <div class="level-right">
+        <a
+          v-if="user.surveyAvailable"
+          class="button is-primary"
+          @click.prevent="startSurveyFor(user.id)">Fill Out</a>
+        <a
+          v-if="! user.surveyAvailable"
+          class="button"
+          @click.prevent="viewSubmissionFor(user.id)">View Submission</a>
+      </div>
+    </div>
+    <pre>{{ users }}</pre>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import QuestionOpen from '@/components/QuestionOpen.vue'
-import QuestionMultipleChoice from '@/components/QuestionMultipleChoice.vue'
-import QuestionScale from '@/components/QuestionScale.vue'
+import { mapState, mapGetters } from "vuex"
 
 export default {
   name: 'home',
-  components: {
-    QuestionOpen,
-    QuestionMultipleChoice,
-    QuestionScale
-  }
+  data() {
+    return {
+    }
+  },
+  computed: {
+    ...mapState(['users', 'surveys']),
+    ...mapGetters(['currentUser'])
+  },
 }
 </script>
