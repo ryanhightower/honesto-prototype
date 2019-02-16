115dede49bd02bdcae9a985bde1090658311a834
Ryan Hightower <ryan@ryanhightower.com>
2019-02-16 14:34:53 -0700

    Use vuex-persist to save to localStorage

--- src/store.js
+++ src/store.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist'
 import { get } from 'lodash'
 import db from './sampleData'
 
 Vue.use(Vuex)
 
+const vuexPersist = new VuexPersist({
+  key: 'my-app',
+  storage: localStorage
+})
+
 export default new Vuex.Store({
+  plugins: [vuexPersist.plugin],
   state: db,
   getters: {
     currentUser: state =>  get(state, 'users.uuid0', { id: '', name: '', surveyAvailable: false }),
