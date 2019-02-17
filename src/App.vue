c24f5d65575fe75a7d034d78dceb4effa70b4151
Ryan Hightower <ryan@ryanhightower.com>
2019-02-17 00:10:15 -0700

    Add nav link to MyFeedback

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
-  <div id="app" class="container">
+  <div id="app">
     <div class="navbar">
-      <!-- <router-link to="/">Home</router-link> | -->
-      <router-link to="/demo">Demo</router-link> |
-      <router-link to="/feedback">Share Feedback</router-link>
+        <div class="container">
+
+        <div class="navbar-brand">
+          <router-link to="/feedback" class="navbar-item">
+            <h1 class="title is-5">Honesto</h1>
+          </router-link>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-start">
+            <div class="navbar-item">
+              <router-link to="/feedback">Share Feedback</router-link>
+            </div>
+            <div class="navbar-item">
+              <router-link to="/my-feedback">My Feedback</router-link>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <div class="container">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -13,5 +31,11 @@
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   color: #2c3e50;
+
+  .navbar {
+    border-bottom: 1px solid #D9DCDE;
+    background-color: #F2F3F4;
+    margin-bottom: 50px;
+  }
 }
 </style>
