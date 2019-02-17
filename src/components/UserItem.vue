5af2547bdfc8b5ba35d565841129ab209fda1107
Ryan Hightower <ryan@ryanhightower.com>
2019-02-17 01:50:06 -0700

    Style userItem

--- src/components/UserItem.vue
+++ src/components/UserItem.vue
@@ -29,3 +29,19 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.user-item.level {
+  cursor: pointer;
+  margin: 0;
+  padding: 20px;
+  border-bottom: 1px solid #D9DCDE;
+
+  &:hover {
+    background-color: #D5B0F21A
+  }
+  // &:not(:last-child) {
+  // }
+}
+
+</style>
